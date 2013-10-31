51b073c87a31ed2025d608ffe6e140edd46860af
v-vltsis <v-vltsis@microsoft.com>
2013-10-31 01:29:20 -0700

    Added loader for svgweb

---
+++ polyfills/svgweb_load.js
@@ -0,0 +1,18 @@
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+function getInternetExplorerVersion()
+{
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
+if ((getInternetExplorerVersion() < 9) || (window.location.search.indexOf('svg.render.forceflash=true') != -1)) {
+  document.write('<script type="text/javascript" src="../../polyfills/svgweb/src/svg.js"></script>');
+}
